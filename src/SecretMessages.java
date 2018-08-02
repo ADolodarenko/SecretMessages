edf80b22ca2b8fc97132935dd24a8a051197ee86
ADolodarenko <adolodar@gmail.com>
2018-08-02 18:22:14 +0300

    First commit.

---
+++ src/SecretMessages.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class SecretMessages
+{
+
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a message for encode or decode:");
+		
+		String message = scan.nextLine();
+		String output = "";
+		char key = 13;
+		
+		for (int x = 0; x < message.length(); x++)
+		{
+			char input = message.charAt(x);
+			
+			if (input >= 'A' && input <= 'Z')
+			{
+				input += key;
+				
+				if (input > 'Z')
+					input -= 26;
+			}
+			else if (input >= 'a' && input <= 'z')
+			{
+				input += key;
+				
+				if (input > 'z')
+					input -= 26;
+			}
+			
+			output += input;
+		}
+		
+		System.out.println(output);
+		
+		scan.close();
+	}
+
+}
