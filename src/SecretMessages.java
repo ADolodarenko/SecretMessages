7301ef33d608333ce6edffc40859bf1db18ee2b5
ADolodarenko <adolodar@gmail.com>
2018-08-03 00:35:14 +0300

    We can set the secret key and cipher digits also.

--- src/SecretMessages.java
+++ src/SecretMessages.java
@@ -10,7 +10,10 @@ public class SecretMessages
 		
 		String message = scan.nextLine();
 		String output = "";
-		char key = 13;
+		
+		System.out.println("Enter a secret key (-25 to 25):");
+		int keyVal = Integer.parseInt(scan.nextLine());
+		char key = (char)keyVal;
 		
 		for (int x = 0; x < message.length(); x++)
 		{
@@ -22,6 +25,8 @@ public class SecretMessages
 				
 				if (input > 'Z')
 					input -= 26;
+				if (input < 'A')
+					input += 26;
 			}
 			else if (input >= 'a' && input <= 'z')
 			{
@@ -29,6 +34,16 @@ public class SecretMessages
 				
 				if (input > 'z')
 					input -= 26;
+				if (input < 'a')
+					input += 26;
+			}
+			else if (input >= '0' && input <= '9')
+			{
+				input += (keyVal % 10);
+				if (input > '9')
+					input -= 10;
+				if (input < '0')
+					input += 10;
 			}
 			
 			output += input;
